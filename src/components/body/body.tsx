c5457f65caf24eb6aae4cac6dd33f18fc503c659
Guilherme <guilherm.rodrigues@outlook.com>
2026-05-03 14:38:43 -0300

    add link a logo

--- src/components/body/body.tsx
+++ src/components/body/body.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react'
 import BotaoAdd from './addButton'
 import Corpo from "./corpo"
 import Link from 'next/link'
+import { Button } from '../ui/button'
 
 export function Body() {
     const [refreshKey, setRefreshKey] = useState(0)
@@ -31,10 +32,12 @@ export function Header() {
         <div>
             <div className="flex items-center justify-between py-4 px-4 md:py-0 md:px-10 md:m-10">
                     <div>
-                        <img src="logo.png" alt="Logo" className="h-30 w-auto" />
+                        <Link href='/'><img src="logo.png" alt="Logo" className="h-30 w-auto" /></Link>
                     </div>
-                    <div className="flex justify-end">
-                        <Link href="/note">Adicionar Nota</Link>
+                    <div className="flex justify-end p-4">
+                        <Button variant="secondary" className="mr-2 px-4 py-2 " >
+                            <Link href="/note">Notas</Link>
+                        </Button>
                         
                         <BotaoAdd onProductAdded={handleProductAdded} />
                     </div>
