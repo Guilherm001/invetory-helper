f4f49f558daaf0f5e49c1d3eb3ef43fdaf0dca66
Guilherme <guilherm.rodrigues@outlook.com>
2026-06-25 11:51:20 -0300

    personalizando notes

--- src/features/notes/components/notas/notas.tsx
+++ src/features/notes/components/notas/notas.tsx
@@ -29,29 +29,42 @@ export default function NoteCard({ title, notes, colorClass, bgclass }: NoteCard
         <p className="text-sm text-gray-400">Nenhuma nota</p>
       ) : (
         notes.map((note) => (
-          <HoverCard key={note.id} openDelay={10} closeDelay={100} >
-            <HoverCardTrigger asChild>
-              <Button variant="ghost" className={`w-full mb-2 ${bgclass}`}>
-                {note.title}
-                
-              </Button>
-            </HoverCardTrigger>
-            <HoverCardTrigger>
-              <Button>
-                tst
-              </Button>
-            </HoverCardTrigger>
-            <HoverCardContent className="flex w-64 flex-col gap-0.5">
-              <div className="font-semibold">
-                Prioridade: <span className={colorClass}>{note.priority}</span>
-              </div>
-              <div>{note.notes}</div>
-            </HoverCardContent>
+          <div className="flex items-center gap-2 mb-2" key={note.id}>
+            <HoverCard key={note.id} openDelay={10} closeDelay={100} >
+              <HoverCardTrigger asChild>
+              
+                  <Button variant="ghost" className={`w-full mb-2 flex-1 ${bgclass}`}>
+                    {note.title}
             
-          </HoverCard>
+                  </Button>
+                
+            
+            
+              </HoverCardTrigger>
+            
+              <HoverCardContent className="flex w-64 flex-col gap-0.5">
+                <div className="font-semibold">
+                  Prioridade: <span className={colorClass}>{note.priority}</span>
+                </div>
+                <div>{note.notes}</div>
+              </HoverCardContent>
+            
+            </HoverCard>
 
-        ))
-      )}
+            <Button size="icon" variant="outline">
+              ✏️
+            </Button>
+                  
+            <Button size="icon" variant="destructive">
+              🗑️
+            </Button>
+                  
+          </div>
+                    
+                  
+       ))
+     )}
     </div>
   );
 }
+
