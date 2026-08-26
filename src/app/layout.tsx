4b1699a528af602c45a990f910c1024a0f8ba6fe
Guilherme <guilherm.rodrigues@outlook.com>
2026-08-26 16:54:07 -0300

    estilizando o Header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,14 +18,29 @@ export default function RootLayout({
       <body>
         <main className="flex flex-col max-w-300 min-w-100 m-auto h-screen bg-white">
           <header className="sticky top-0 z-20 border-b border-gray-200 bg-white">
-            <div className="flex items-center justify-between px-4 py-4 md:px-10">
+            <div className="flex items-center  ">
               <Link href="/">
                 <img src="/logo.png" alt="Logo" className="h-40 w-auto" />
               </Link>
-              <nav className="flex items-center gap-3">
+              <nav className="flex border-b border-gray-200 bg-white">
+                <Link 
+                  href="/"
+                  className=" relative flex flex-1 items-center justify-center gap-2
+                    px-6 py-4 text-base font-medium text-[#079C9C]
+                    transition-colors duration-200
+                    hover:bg-[#079C9C]/5
+                    after:absolute after:bottom-0 after:left-0
+                    after:h-[3px] after:w-full
+                    after:rounded-t-full after:bg-[#079C9C]"
+                >
+                  Produtos
+                </Link>
                 <Link
                   href="/note"
-                  className="rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+                  className="flex flex-1 items-center justify-center gap-2
+                    px-6 py-4 text-base font-medium text-slate-500
+                    transition-colors duration-200
+                    hover:bg-slate-50 hover:text-[#079C9C]"
                 >
                   Notas
                 </Link>
